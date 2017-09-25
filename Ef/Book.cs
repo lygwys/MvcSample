ca7ba6885f9902ccc4825e15b4de4b82b6d8bd46
lygwys <lygwys@outlook.com>
2017-09-25 16:27:45 +0800

    Initial commit

---
+++ Ef/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcSample.Ef
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Ef/MvcDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MvcSample.Ef
+{
+    public class MvcDbContext:DbContext
+    {
+        public MvcDbContext() : base("myConn") { }//传入Configuration中连接字串名字
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)//移除约定 让迁移的表名为单数形式，默认是以Books命名数据表
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
